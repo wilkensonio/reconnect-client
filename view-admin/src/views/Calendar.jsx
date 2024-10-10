1db0688b0e0edfd11400e554563b7341202a65e9
Mitchell DeCesare <mitchelldc18@gmail.com>
2024-10-10 11:43:34 -0400

    Added Previous and Next Year buttons on the calendar

--- view-admin/src/views/Calendar.jsx
+++ view-admin/src/views/Calendar.jsx
@@ -1,105 +1,29 @@
 import React from 'react';
-import Calendar from 'react-lightweight-calendar';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import bootstrapPlugin from '@fullcalendar/bootstrap';
+import './Calendar.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap styles
 
-const MyCalendar = () => {
-  const [currentDate, setCurrentDate] = React.useState('2023-06-02');
+export default function Calendar() {
   return (
-    <Calendar
-      data={[
-        {
-          id: '1',
-          startTime: '2023-06-02T01:10:00Z',
-          endTime: '2023-06-02T03:10:00Z',
-          title: 'Conference',
-        },
-        {
-          id: '2',
-          startTime: '2023-06-02T01:00:00Z',
-          endTime: '2023-06-02T02:00:00Z',
-          title: 'Meet up',
-        },
-        {
-          id: '3',
-          startTime: '2023-06-02T04:20:00Z',
-          endTime: '2023-06-02T05:20:00Z',
-          title: 'Codefair',
-          bgColor: 'rgb(129, 205, 242)',
-          textColor: 'white',
-        },
-      ]}
-      currentView='WEEK_TIME'
-      currentDate={currentDate}
-      setCurrentDate={setCurrentDate}
-      activeTimeDateField='startTime-endTime' // Or just startTime or just endTime
-      weekStartsOn={1} // Monday
-      // If you want additional customization, uncomment the code and make the appropriate changes
-      // renderItem={(data, isHovered) => {
-        // Custom rendering of event element
-        // return (
-              // <>
-                // <div>{data.title}</div>
-                // <div>{data.id}</div>
-              // </>
-          // )
-        // console.log(data, isHovered);
-      // }}
-      // renderItemText={(data) => {
-        // Custom rendering of event element text
-        // return <p>{data.title}</p>;
-      // }}
-      // renderHeaderItem={(data, extras) => {
-        // Custom rendering of header element
-        // return <div>{data.startTime}-{date.endTime}</div>;
-      // }}
-      // renderHeaderItemText={(data) => {
-        // Custom rendering of header element text
-        // return <p>{data.title}</p>;
-      // }}
-      enableHoverEffect={true}
-      colorDots={[
-        {
-          color: 'red',
-          text: 'Busy day',
-          date: '2023-06-02',
-        },
-      ]}
-      timeDateFormat={{
-        day: 'EEEE',
-        hour: 'hh a',
-        monthYear: 'LLLL yyyy',
-      }}
-      onDayNumberClick={(day) => {
-        // Handle day number click
-        console.log(day);
-      }}
-      onDayStringClick={(day) => {
-        // Handle day text click
-        console.log(day);
-      }}
-      onHourClick={(value) => {
-        // Handle hour click
-        console.log(value);
-      }}
-      onColorDotClick={(value) => {
-        // Handle color dot click
-        console.log(value);
-      }}
-      onItemClick={(item) => {
-        // Handle event item click
-        console.log(item);
-      }}
-      onCellClick={(value) => {
-        // Handle cell click
-        console.log(value);
-      }}
-      cellDisplayMode={{
-        WEEK_TIME: {
-          inactiveCells: ['2023-05-29'],
-          state: 'CUSTOM',
-        },
-      }}
-    />
-  );
-};
-      
-export default MyCalendar;
+    <div className="container mt-4">
+    <div className="row justify-content-center">
+        <FullCalendar
+          plugins={[dayGridPlugin, bootstrapPlugin]}
+          initialView="dayGridMonth"
+          headerToolbar={{
+            start: 'prevYear', 
+            center: 'title',
+            // left: 'today prev next', 
+            end: 'nextYear', 
+           
+          }}
+          
+          themeSystem="bootstrap" // Use Bootstrap theme for FullCalendar
+        />
+      </div>
+    </div>
+
+  )
+}
