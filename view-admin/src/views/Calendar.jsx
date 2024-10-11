f3151cf75a02b012a624e0aafd5ab21da5b5399a
Mitchell DeCesare <mitchelldc18@gmail.com>
2024-10-10 21:45:02 -0400

    Added handleDateClick

--- view-admin/src/views/Calendar.jsx
+++ view-admin/src/views/Calendar.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import bootstrapPlugin from '@fullcalendar/bootstrap';
+import interactionPlugin from '@fullcalendar/interaction';
 import './Calendar.css'
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap styles
 
 export default function Calendar() {
+  const [events, setEvents] = useState([]);
+
+  const handleDateClick = (arg) => {
+    const title = prompt('Enter event title:'); 
+    if (title) {
+      setEvents((prevEvents) => [
+        ...prevEvents,
+        {
+          id: String(prevEvents.length + 1), 
+          title,
+          start: arg.dateStr, 
+          end: arg.dateStr,
+          
+        },
+      ]);
+    }
+  };
+
   return (
     <div className="container mt-4">
     <div className="row justify-content-center">
         <FullCalendar
-          plugins={[dayGridPlugin, bootstrapPlugin]}
+          plugins={[dayGridPlugin, bootstrapPlugin, interactionPlugin]}
           initialView="dayGridMonth"
           headerToolbar={{
             left: 'prev,next today',
             center: 'title',
-            right: 'dayGridMonth,dayGridWeek,dayGridDay'
+            right: 'dayGridMonth,dayGridWeek,dayGridDay',
           }}
 
           buttonText= {{
@@ -26,8 +45,10 @@ export default function Calendar() {
             week: 'Week',
             day: 'Day'
           }}
-          
+
+   
           themeSystem="bootstrap" // Use Bootstrap theme for FullCalendar
+          dateClick={handleDateClick} 
         />
       </div>
     </div>
