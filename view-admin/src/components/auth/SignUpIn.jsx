93a10aafd67f80be691c662992fe88268b28645d
Wilkenson <wilkensoncode@gmail.com>
2024-10-19 11:27:19 -0400

    Dissable button for sign in and sign up if fields are not filled out

--- view-admin/src/components/auth/SignUpIn.jsx
+++ view-admin/src/components/auth/SignUpIn.jsx
@@ -4,7 +4,7 @@ import SignInForm from '../../views/SignInForm';
 import SignUpForm from '../../views/SignUpForm';
 import ResetPasswordModal from './ResetPasswordModal';
 import VerifyEmailModal from './VerifyEmailModal';
-import {CCard, CCardBody, CCardHeader, CCol, CRow } from '@coreui/react';
+import {CButton, CCard, CCardBody, CCardHeader, CCol, CRow, CTooltip } from '@coreui/react';
  
 
 function SignUpIn() {
@@ -54,8 +54,39 @@ function SignUpIn() {
                     transition: 'filter 0.3s',   
                 }}>
                 <CCardHeader style={{ borderBottom: 'none',  background: '#e9e9e9' }}> 
-                    <p className='h3 d-flex justify-content-center mt-3 mb-5'>{isLogin ? 'Sign In' : 'Sign Up'}</p>
-                    <SwitchForm isLogin={isLogin} handleSwitch={handleSwitch} />
+                    <div className="">    
+                        <p className='h3 d-flex justify-content-center mt-3 mb-5'>
+                            {isLogin ? (
+                                <>
+                                    Sign In{'  '}
+                                    <CTooltip content="To Reset password clicked on the reset password link on this page
+                                        You will see a popup to enter your email address and new password. 
+                                        Then click Next and you will receive a code though email to reset your password."
+                                         placement="bottom">
+                                        <span className="border border-primary bg-tooltip rounded-circle text-white d-flex justify-content-center align-items-center"
+                                            style={{ cursor: 'pointer', color: '#007bff', marginLeft: '10px', width:'2rem', height: '2rem', paddingTop:'.2rem' }}>
+                                            ?
+                                        </span>
+                                    </CTooltip>
+                                </>) : 
+                                <>
+                                    Sign Up{'  '}
+                                    <CTooltip content="Next button is not active until
+                                     all field are validated. password must be 8 characters long, 
+                                     with at least one uppercase letter and one number.
+                                     Once you click on next, you will receive an email to verify your account." 
+                                     placement="bottom">
+                                        <span className="border border-primary bg-tooltip rounded-circle text-white d-flex justify-content-center align-items-center"
+                                            style={{ cursor: 'pointer', color: '#007bff', marginLeft: '10px', width:'2rem', height: '2rem', paddingTop:'.2rem' }}>
+                                                ?
+                                        </span>
+                                    </CTooltip>
+                                </>
+                            }
+                          
+                        </p>
+                    </div>
+                        <SwitchForm isLogin={isLogin} handleSwitch={handleSwitch} />
                 </CCardHeader>
                 <CCardBody className="mt-5" style={{marginBottom:'35px !importan'}}> 
                     <CRow>
--- view-admin/src/views/SignInForm.jsx
+++ view-admin/src/views/SignInForm.jsx
@@ -3,6 +3,7 @@ import { CButton, CRow, CCol, CForm, CFormInput } from '@coreui/react';
 import { useLocation } from 'react-router-dom';
 import {signinUser} from '../ApiService/SignService';
 import {getToken} from '../ApiService/TokenService';
+import useFormValidation from '../components/validButton/useFormValidation';
 
 function SignInForm({onResetPassword}) {
   const [email, setEmail] = useState('');
@@ -12,6 +13,23 @@ function SignInForm({onResetPassword}) {
   const [error, setError] = useState('');
   const [message, setMessage] = useState('');
   const location = useLocation();  
+
+  const validateFields = (fields) => {
+    const {email, password} = fields;
+    return (
+      email.trim() !== '' &&
+      email.toLocaleLowerCase().endsWith
+      ('@southernct.edu') &&
+      password.trim() !== ''
+    );
+  };
+
+
+  const isFormValid = useFormValidation(
+    {email, password},
+    validateFields
+  );
+
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
@@ -106,7 +124,11 @@ function SignInForm({onResetPassword}) {
               />
             </CCol>
             <CCol xs={12} className='mt-3'>
-              <CButton className='ccolor' type="submit" style={{position: 'relative', top:'10px'}}>Signin</CButton>
+              <CButton className='ccolor' type="submit" 
+                style={{position: 'relative', top:'10px'}}
+                disabled={!isFormValid}
+              > Signin
+              </CButton>
               <div className="mt-5 d-flex justify-content-center mb-3"> 
                 <a as button className="text-decoration-none" onClick={handleResetPasswordClick} style={{cursor: 'pointer'}}>Reset password</a>
               </div>
