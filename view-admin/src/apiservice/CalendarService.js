8f171cef29c4e09d5201fae7c91f36e0741d022d
Mitchell DeCesare <mitchelldc18@gmail.com>
2024-11-01 23:45:29 -0400

    modal appointment display

--- view-admin/src/apiservice/CalendarService.js
+++ view-admin/src/apiservice/CalendarService.js
@@ -20,3 +20,36 @@ export const getAllAppointments = async (user_id) => {
     throw error.response?.data || new Error('Signout failed');
   }
 }
+
+export const updateAppointment = async (data) => {
+  try {
+    const response = await axios.put(`/api/appointment/update/${data.user_id}`, data, {
+      headers: {
+          'R-API-KEY': `${apiKey}`,
+          'Content-Type': 'application/json',
+          'Authorization': `${token_type} ${token}`
+      },
+    });
+    
+    return response;
+  } catch (error) {  
+    throw error.response?.data || new Error('Failed to delete appointment'); 
+}      
+}
+
+export const deleteAppointment = async (id) => {
+  try {
+      const response = await axios.delete(`/api/appointment/delete/${id}`, {
+          headers: {
+              'R-API-KEY': `${apiKey}`,
+              'Content-Type': 'application/json',
+              'Authorization': `${token_type} ${token}`
+          },
+      }); 
+      
+      return response.data;
+               
+  } catch (error) {  
+       throw error.response?.data || new Error('Failed to delete appointment'); 
+  }      
+}
